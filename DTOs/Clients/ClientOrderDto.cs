dea8a29618eca40e3023547c2cad27efbbf7c460
Sebastian Gutierrez <rodrigo.gutierrez.v@tecsup.edu.pe>
2026-05-15 14:41:31 -0500

    Se añade nuevos codigos lb09 parte 2

---
+++ DTOs/Clients/ClientOrderDto.cs
@@ -0,0 +1,9 @@
+﻿using lab08_SebastianGutierrez.DTOs.Orders;
+
+namespace lab08_SebastianGutierrez.DTOs.Clients;
+
+public class ClientOrderDto
+{
+    public string ClientName { get; set; } = null!;
+    public List<OrderDto> Orders { get; set; } = new List<OrderDto>();
+}
---
+++ DTOs/Orders/OrderDto.cs
@@ -0,0 +1,7 @@
+﻿namespace lab08_SebastianGutierrez.DTOs.Orders;
+
+public class OrderDto
+{
+    public int OrderId { get; set; }
+    public DateTime OrderDate { get; set; }
+}
